76ee6b536b26ab3b1b52f976a72040f45d752301
Loris Leiva <loris.leiva@gmail.com>
2018-09-16 17:54:40 +0200

    :ambulance: Add auth middleware to event routes

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         $events = auth()->user()->events()
