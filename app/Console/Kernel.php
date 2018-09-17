d06b8a88c91c61b813c61951040734ce7e250045
Loris Leiva <loris.leiva@gmail.com>
2018-09-17 19:03:52 +0200

    :sparkles: Schedule periodic synchronizations

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\PeriodicSynchronizations;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->job(new PeriodicSynchronizations())->everyFifteenMinutes();
     }
 
     /**
---
+++ app/Jobs/PeriodicSynchronizations.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Synchronization;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class PeriodicSynchronizations implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {   
+        Synchronization::get()->each->ping();
+    }
+}
